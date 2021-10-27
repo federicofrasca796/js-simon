360955b9795ec994f9eec294c99f00eec570db9c
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-27 15:28:02 +0200

    add html struct | add js instructions & logic

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* Descrizione
+Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+
+
+/* RANDOM NUMBERS MUST BE STORED TO LATER CHECK USER MEMORY SKILLS  */
+//generate random numbers
+
+//push random numbers in array
+
+//show array numbers in alert()
+//alert must be visibile for 30sec
+//show 30sec counter? OPTIONAL
+
+
+
+
+//ask user for the same 5 numbers
+//store user input in array
+
+//filter array for matching numbers. Exclude wrong guesses.
+
+//show resulting array.length and values.
+
+
