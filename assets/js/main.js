6365d53796a0629ce882ccc0e28eb43fce2a1c97
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-27 18:49:47 +0200

    add results and timer to DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,8 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 
 //DOM elements
-
+const timerEl = document.querySelector('.timer');
+const resultEl = document.querySelector('.result');
 
 /* RANDOM NUMBERS MUST BE STORED TO LATER CHECK USER MEMORY SKILLS  */
 // alert(genRandomArr());
@@ -22,13 +23,18 @@ let i = 1;
 let userArr = [];
 
 const timer = setInterval(() => {
-    console.log(i);
+    timerEl.innerHTML = i;
     if (i === 0){
+        //show 30sec counter
+        timerEl.style.display = 'none';
         //stop timer
         clearInterval(timer);
+        
 
+        /* ASK & CHECK USER INPUT */
         //time's up. Get 5 inputs from user
         for (let i = 0; i < randomArr.length; i++) {
+            //ask user for nums
             const userNum = parseInt(prompt('Scrivi un numero che ricordi di quelli che hai visto.')); 
             
             //store user input in array if matching with randomArray
@@ -48,31 +54,23 @@ const timer = setInterval(() => {
         
         console.log(userArr);
 
-        alert(`Hai ricordato correttamente ${userArr.length} numeri!`);
-        alert(`Questi sono i numeri indovinati: ${userArr}`);
-
+        //show resulting array.length and values.
+        resultEl.innerHTML = `Hai ricordato correttamente ${userArr.length} numeri! <br>
+        Questi sono i numeri indovinati: ${userArr}`
     }
     
     i--
 }, 1000);
 
 
-//alert must be visibile for 30sec then it shall be overwrited by prompt.
-//show 30sec counter? OPTIONAL
 
 
 
 
 
 
-/* ASK & CHECK USER INPUT */
-//ask user for the same 5 numbers after 30 secs
-// setTimeout(function(){prompt('Riscrivi i numeri che hai visto, separati da una virgola.');}, 3000)
-//store user input in array
 
-//filter array for matching numbers. Exclude wrong guesses.
 
-//show resulting array.length and values.
 
 
 
